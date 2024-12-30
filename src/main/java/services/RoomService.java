94ddeb846ab77d6af8199d467057732c35ed105b
Aayush-Project-stack <shukla.aayush1012@gmail.com>
2024-12-30 15:29:35 +0530

    My hotel app booking commit 1

---
+++ src/main/java/services/RoomService.java
@@ -0,0 +1,14 @@
+package services;
+
+import models.Hotel;
+
+public class RoomService {
+    
+
+
+   public boolean checkAvailableRooms(Hotel hotel)
+   {
+       return hotel.getAvailableRooms() >= 0;
+   }
+
+}
